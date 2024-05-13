851650fec62bf7baa09510aec035822de4e3a5da
jasiery <jasieryybnz@gmail.com>
2024-05-13 22:18:02 +0800

    initial commit(structure): project build, maven, springboot
    Using:
    - Java 8
    - Maven 3.6.9
    - Maven Plugin 2.6.0
    - Spring boot 2.6.0

---
+++ src/test/java/com/ybanez/microservice/api/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ybanez.microservice.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
